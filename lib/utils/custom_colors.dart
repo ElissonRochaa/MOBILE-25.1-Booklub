2d2907b241669ae9dc5d2d3c1c0fd33027901f72
Luan-Vn4 <luanvilaca1@gmail.com>
2025-04-20 21:43:52 -0300

    chore: adicionadas cores personalizadas do app

---
+++ lib/utils/custom_colors.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+abstract final class CustomColors {
+
+  static const Color darkViolet = Color.fromARGB(255, 29, 32, 124)
+
+  static const Color violet = Color.fromARGB(255, 47, 52, 145);
+
+  static const Color lightViolet = Color.fromARGB(255, 72, 76, 145);
+
+  static const Color darkVioletBlue = Color.fromARGB(255, 74, 95, 206);
+
+  static const Color violetBlue = Color.fromARGB(255, 89, 102, 177);
+
+  static const Color lightVioletBlue = Color.fromARGB(255, 118, 127, 174);
+
+  static const Color green = Color.fromARGB(255, 52, 199, 89);
+
+  static const Color red = Color.fromARGB(255, 242, 2, 2 );
+
+  static const Color white = Color.fromARGB(255, 245, 245, 245);
+
+  static const Color darkWhite = Color.fromARGB(255, 207, 207, 207);
+
+  static const Color superDarkWhite = Color.fromARGB(255, 175, 175, 175);
+
+  static const Color black = Color.fromARGB(255, 73, 73, 73);
+
+  static const Color lightBlack = Color.fromARGB(255, 116, 116, 116);
+
+  static const Color superLightBlack = Color.fromARGB(255, 154, 154, 154);
+  
+} 
