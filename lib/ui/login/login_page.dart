be2275a7e3cce3692cee934dd3b3315517a10576
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-04-26 17:57:47 -0300

    refactor: refatorando as páginas de login e register pra acomodarem a lógica de validação

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -1,57 +1,94 @@
+import 'package:booklub/ui/core/view_models/input_field_validation.dart';
 import 'package:booklub/ui/core/widgets/buttons/purple_rounded_button.dart';
 import 'package:booklub/ui/core/widgets/text_input_fields/text_field_password.dart';
 import 'package:booklub/ui/core/widgets/text_input_fields/text_field_with_field_name.dart';
 import 'package:booklub/ui/login/widgets/cadastrar_clickable_text.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final InputFieldValidation fieldValidatorModel = InputFieldValidation();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned.fill(
-            child: Opacity(
-              opacity: 0.5,
-              child: Image.asset(
-                'assets/images/light-background.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
+          _buildBackground(),
           Center(
             child: FractionallySizedBox(
               widthFactor: 0.7,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/Booklub_logo.png',
-                    height: MediaQuery.sizeOf(context).height * 0.28,
-                  ),
-                  SizedBox(height: 30),
-                  Column(
-                    children: [
-                      TextFieldWithFieldName("E-mail"),
-                      SizedBox(height: 16),
-                      TextFieldPassword(),
-                      SizedBox(height: 16),
-                    ],
-                  ),
-                  SizedBox(height: 30),
-                  Column(
-                    children: [
-                      PurpleRoundedButton("Entrar", () => ()), //Tenho que adicionar logica de validação aqui e posteriormente api call
-                      SizedBox(height: 16),
-                      CadastrarClickableText(),
-                    ],
-                  ),
-                ],
-              ),
+              child: _buildLoginForm(context),
             ),
           ),
         ],
       ),
     );
+  }
+
+  Widget _buildBackground() {
+    return Positioned.fill(
+      child: Opacity(
+        opacity: 0.5,
+        child: Image.asset(
+          'assets/images/light-background.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginForm(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildLogo(context),
+        const SizedBox(height: 30),
+        _buildTextFields(),
+        const SizedBox(height: 30),
+        _buildButtons(),
+      ],
+    );
+  }
+
+  Widget _buildLogo(BuildContext context) {
+    return Image.asset(
+      'assets/images/Booklub_logo.png',
+      height: MediaQuery.sizeOf(context).height * 0.28,
+    );
+  }
+
+  Widget _buildTextFields() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFieldWithFieldName("E-mail", fieldValidatorModel.validateEmail),
+          SizedBox(height: 16),
+          TextFieldPassword(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    return Column(
+      children: [
+        PurpleRoundedButton("Entrar", () => _submitForm()),
+        SizedBox(height: 16),
+        CadastrarClickableText(),
+      ],
+    );
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState?.validate() ?? false) {
+      // Api call aqui qnd a gnt for itnegrar
+    }
   }
 }
--- lib/ui/register/register_page.dart
+++ lib/ui/register/register_page.dart
@@ -1,78 +1,132 @@
+import 'package:booklub/ui/core/view_models/input_field_validation.dart';
 import 'package:booklub/ui/core/widgets/buttons/purple_rounded_button.dart';
 import 'package:booklub/ui/core/widgets/text_input_fields/text_field_password.dart';
 import 'package:booklub/ui/core/widgets/text_input_fields/text_field_with_field_name.dart';
-import 'package:booklub/ui/login/widgets/cadastrar_clickable_text.dart';
 import 'package:booklub/ui/register/widgets/logar_clickable_text.dart';
 import 'package:flutter/material.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
+  final InputFieldValidation fieldValidatorModel = InputFieldValidation();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Stack(children: [_buildBackground(), _buildForm(context)]),
+    );
+  }
+
+  Widget _buildBackground() {
+    return Positioned.fill(
+      child: Opacity(
+        opacity: 0.5,
+        child: Image.asset(
+          'assets/images/light-background.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildForm(BuildContext context) {
+    return Center(
+      child: FractionallySizedBox(
+        widthFactor: 0.7,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _buildLogo(),
+            SizedBox(height: 25),
+            _buildTextFields(),
+            SizedBox(height: 30),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return Image.asset(
+      'assets/images/booklub_logo_icon.png',
+      height: MediaQuery.sizeOf(context).height * 0.20,
+    );
+  }
+
+  Widget _buildButtons() {
+    return Column(
+      children: [
+        PurpleRoundedButton("Cadastrar", () => _submitForm()),
+        SizedBox(height: 16),
+        LogarClickableText(),
+      ],
+    );
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState?.validate() ?? false) {
+      // Api call aqui qnd a gnt for itnegrar
+    }
+  }
+
+  Widget _buildTextFields() {
+    return Form(
+      key: _formKey,
+      child: Column(
         children: [
-          Positioned.fill(
-            child: Opacity(
-              opacity: 0.5,
-              child: Image.asset(
-                'assets/images/light-background.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Center(
-            child: FractionallySizedBox(
-              widthFactor: 0.7,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/booklub_logo_icon.png',
-                    height: MediaQuery.sizeOf(context).height * 0.20,
-                  ),
-                  SizedBox(height: 25),
-                  Column(
-                    children: [
-                      TextFieldWithFieldName("Nome"),
-                      SizedBox(height: 16),
-                      TextFieldWithFieldName("Sobrenome"),
-                      SizedBox(height: 16),
-                      Row(
-                        children: [
-                          Expanded(
-                            flex: 3,
-                            child: TextFieldWithFieldName("Usuário"),
-                          ),
-                          SizedBox(width: 16),
-                          Expanded(
-                            flex: 2,
-                            child: TextFieldWithFieldName("Nascimento"),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 16),
-                      TextFieldWithFieldName("E-mail"),
-                      SizedBox(height: 16),
-                      TextFieldPassword(),
-                    ],
-                  ),
-                  SizedBox(height: 30),
-                  Column(
-                    children: [
-                      PurpleRoundedButton(
-                        "Cadastrar",
-                        () => (),
-                      ), //Tenho que adicionar logica de validação aqui e posteriormente api call
-                      SizedBox(height: 16),
-                      LogarClickableText(),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
+          _buildNameFields(),
+          SizedBox(height: 16),
+          _buildDateAndUserFields(),
+          SizedBox(height: 16),
+          TextFieldWithFieldName("E-mail", fieldValidatorModel.validateEmail),
+          SizedBox(height: 16),
+          TextFieldPassword(),
         ],
       ),
     );
   }
+
+  Widget _buildNameFields() {
+    return Column(
+      children: [
+        TextFieldWithFieldName(
+          "Nome",
+          fieldValidatorModel.validateBasicTextField,
+        ),
+        SizedBox(height: 16),
+        TextFieldWithFieldName(
+          "Sobrenome",
+          fieldValidatorModel.validateBasicTextField,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDateAndUserFields() {
+    return Row(
+      children: [
+        Expanded(
+          flex: 3,
+          child: TextFieldWithFieldName(
+            "Usuário",
+            fieldValidatorModel.validateBasicTextField,
+          ),
+        ),
+        SizedBox(width: 16),
+        Expanded(
+          flex: 2,
+          child: TextFieldWithFieldName(
+            "Nascimento",
+            fieldValidatorModel.validateBasicTextField,
+          ),
+        ),
+      ],
+    );
+  }
+
 }
