eafa52aca1e92dad9baa88fa4a904a9ee9b17642
Michael Albuquerque <michael.albuquerque.dos.santos@hotmail.com>
2025-04-26 11:38:13 -0300

    feat: adicionando widget referente ao texto clicável pra redirecionar user pra página de cadastro. não linkei o texto a página de cadastro ainda.

---
+++ lib/ui/login/widgets/cadastrar_clickable_text.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class CadastrarClickableText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    TextStyle defaultStyle = TextStyle(
+      color: theme.colorScheme.onSurface,
+      fontSize: theme.textTheme.bodyLarge?.fontSize ?? 16,
+    );
+    TextStyle linkStyle = TextStyle(color: theme.colorScheme.secondary);
+    return RichText(
+      text: TextSpan(
+        style: defaultStyle,
+        children: <TextSpan>[
+          TextSpan(text: 'ou '),
+          TextSpan(
+            text: 'cadastre-se',
+            style: linkStyle,
+            recognizer:
+                TapGestureRecognizer()
+                  ..onTap = () {
+                    context.go('/'); //Aqui eu vou fazer quando adicionar a página de registrar
+                  },
+          ),
+        ],
+      ),
+    );
+  }
+}
