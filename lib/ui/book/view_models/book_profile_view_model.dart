bcafb83322b296aa9e3c37e21516c15808881dd1
holyvieri <beatriz.oliveirac@upe.br>
2025-06-18 00:59:21 -0300

    refactor(book profile): extends async change notifier

--- lib/ui/book/view_models/book_profile_view_model.dart
+++ lib/ui/book/view_models/book_profile_view_model.dart
@@ -1,8 +1,9 @@
+import 'package:booklub/ui/core/view_models/async_change_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:booklub/domain/entities/books/book_item.dart';
 import 'package:booklub/infra/books/book_api_repository.dart';
 
-class BookProfileViewModel extends ChangeNotifier {
+class BookProfileViewModel extends AsyncChangeNotifier<BookItem> {
   final BookApiRepository _bookRepository;
 
   BookItem? _book;
@@ -12,9 +13,28 @@ class BookProfileViewModel extends ChangeNotifier {
   BookProfileViewModel({required BookApiRepository bookRepository})
       : _bookRepository = bookRepository;
 
-Future<void> setBook(String volumeId) async {
-  _book = await _bookRepository.getBookById(volumeId);
-  notifyListeners();
-}
+  @override
+  BookItem? get payload => _book;
 
+  Future<void> setBook(String volumeId) async {
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      _book = await _bookRepository.getBookById(volumeId);
+      error = null;
+    } catch (e, trace) {
+      error = (object: e, stackTrace: trace);
+      _book = null;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void checkBookLoaded() {
+    if (_book == null) {
+      throw StateError('Book not loaded');
+    }
+  }
 }
