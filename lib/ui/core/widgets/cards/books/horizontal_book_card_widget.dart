86593e34464e284d80c84a2f6eaf2ec52c01d72d
holyvieri <beatriz.oliveirac@upe.br>
2025-06-19 20:39:22 -0300

    fix: estouro de titulo no card

--- lib/ui/core/widgets/cards/books/horizontal_book_card_widget.dart
+++ lib/ui/core/widgets/cards/books/horizontal_book_card_widget.dart
@@ -1,7 +1,7 @@
-import 'package:booklub/config/routing/routes.dart';
 import 'package:booklub/domain/entities/books/book_item.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:booklub/config/routing/routes.dart';
 
 class HorizontalBookCardWidget extends StatelessWidget {
   final BookItem book;
@@ -10,18 +10,11 @@ class HorizontalBookCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final colorScheme = Theme.of(context).colorScheme;
-
-    final borderRadius = BorderRadius.only(
-      topLeft: Radius.circular(100),
-      topRight: Radius.circular(36),
-      bottomLeft: Radius.circular(100),
-      bottomRight: Radius.circular(36),
-    );
+    final borderRadius = BorderRadius.circular(12);
 
     final bookCover = ClipRRect(
-      borderRadius: BorderRadius.circular(12),
+      borderRadius: BorderRadius.circular(8),
       child: Image.network(
         book.thumbnail ?? '',
         height: 100,
@@ -37,18 +30,26 @@ class HorizontalBookCardWidget extends StatelessWidget {
       ),
     );
 
-    final bookInfo = Expanded(
+    final bookInfo = Flexible(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Text(
-              book.title,
-              style: Theme.of(context).textTheme.titleMedium,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    book.title,
+                    style: Theme.of(context).textTheme.titleMedium,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    softWrap: true,
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: 4),
             Row(
@@ -68,7 +69,7 @@ class HorizontalBookCardWidget extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(top: 4),
                 child: Text(
-                  book.datePublished!.substring(0, 4), // mostra só o ano
+                  book.datePublished!.substring(0, 4),
                   style: Theme.of(context).textTheme.bodySmall?.copyWith(
                     color: colorScheme.onSurface.withOpacity(0.6),
                   ),
@@ -85,13 +86,16 @@ class HorizontalBookCardWidget extends StatelessWidget {
         context.push(Routes.individualBook(bookId: id));
       },
       borderRadius: borderRadius,
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: borderRadius),
-        child: Row(
-          children: [
-            Padding(padding: const EdgeInsets.all(8), child: bookCover),
-            bookInfo,
-          ],
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 350),
+        child: Card(
+          shape: RoundedRectangleBorder(borderRadius: borderRadius),
+          child: Row(
+            children: [
+              Padding(padding: const EdgeInsets.all(8), child: bookCover),
+              bookInfo,
+            ],
+          ),
         ),
       ),
     );
